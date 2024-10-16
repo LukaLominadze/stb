b5d3c53464c9477a52b60d7cda92eba55a49ab5a
MustachoMan <fguru974@gmail.com>
2024-10-16 21:26:42 +0400

    adding build file

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+project "STB"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"*.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
